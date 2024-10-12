0472de1c5227afc5c85b72f294e5eb07d344255a
huuthocse <huutho.cse@gmail.com>
2024-10-12 15:27:45 +0700

    Udp

--- Website/resources/views/dashboard.blade.php
+++ Website/resources/views/dashboard.blade.php
@@ -11,3 +11,23 @@ Bảng điều khiển
     <input type="text" value="1">
 </form>
 @endsection
+
+@extends('main')
+
+@section('title')
+Quản sức khỏe
+@endsection
+
+@section('content')
+<h1>List of Goats</h1>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Age</th>
+    </tr>
+    <tr>
+        <td>1</td>
+        <td>25</td>
+    </tr>
+</table>
+@endsection
