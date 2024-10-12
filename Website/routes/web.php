12487ee1f7b2536136900b18c9db38ab38bf4cbc
ThanhKien109 <kienbui123k@gmail.com>
2024-10-12 15:35:11 +0700

    Udp

--- Website/routes/web.php
+++ Website/routes/web.php
@@ -1,13 +1,9 @@
 <?php
-
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\HealthCheckController;
-
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', ['DashboardController', 'getViewDashboard'])->name('home');
-
-Route::get('/health-check', 'App\Http\Controllers\HealthCheckController@getView')->name('health-check');
-
-// Route::get('/health-check', ['HealthCheckController::class', 'getViewQLD'])->name('health-check');
+Route::get('/', 'App\Http\Controllers\DashboardController@getViewDashboard')->name('home');
+Route::get('/danhsachde', 'App\Http\Controllers\DashboardController@getViewQLD')->name('quanlyde');
+Route::get('/login', 'App\Http\Controllers\LoginController@getView')->name('login');
+Route::post('/login', 'App\Http\Controllers\LoginController@login')->name('login_post');
