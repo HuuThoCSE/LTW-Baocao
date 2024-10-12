b5ce15d7460b14d5833d45c8fd9ea1f60936b9cd
HoaiSang789 <sangv123543@gmail.com>
2024-10-12 17:50:17 +0700

    .

--- Website/resources/views/listgoat.blade.php
+++ Website/resources/views/listgoat.blade.php
@@ -6,14 +6,25 @@ Danh sách dê
 
 @section('content')
 <h1>List of Goats</h1>
-<table >
-<ul>
+<table class="table">
+    <tr>
+        <th>Code</th>
+        <th>Tên</th>
+        <th>Tuổi</th>
+        <th>Origin</th>
+    </tr>
+    <ul>
     @foreach($listgoats as $goat)
     
-        <li>{{$goat->goat_id}}-{{ $goat->goat_name }} - {{$goat->goat_age}} - Origin: {{ $goat->origin }}</li>
+        <tr>
+            <td>{{ $goat->goat_id }}</td>
+            <td>{{ $goat->goat_name }}</td>
+            <td>{{ $goat->goat_age }}</td>
+            <td>{{ $goat->origin }}</td>
+        </tr>
    
         
     @endforeach
-</ul>
+    </ul>
 </table>
 @endsection
