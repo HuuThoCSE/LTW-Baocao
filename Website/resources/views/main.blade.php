b7949d0844088137c15802de8134e14c6682a468
ThanhKien109 <kienbui123k@gmail.com>
2024-10-26 22:27:42 +0700

    .

--- Website/resources/views/main.blade.php
+++ Website/resources/views/main.blade.php
@@ -194,52 +194,60 @@
         </li><!-- End Messages Nav -->
 
         <li class="nav-item dropdown pe-3">
-    @if(auth()->check()) <!-- Kiểm tra xem người dùng đã đăng nhập chưa -->
-        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+    
+          <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
             <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">{{ auth()->user()->name }}</span> <!-- Hiển thị tên người dùng -->
-        </a>
-        <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-            <li class="dropdown-header">
-                <h6>Meta</h6>
-                <span>{{ auth()->user()->name }}</span> <!-- Hiển thị tên người dùng -->
-            </li>
-            <li>
-                <hr class="dropdown-divider">
-            </li>
-            <li>
-                <a class="dropdown-item d-flex align-items-center" href="{{ route('profile') }}">
-                    <i class="bi bi-person"></i>
-                    <span>My Profile</span>
-                </a>
-            </li>
-            <li>
-                <hr class="dropdown-divider">
-            </li>
-            <li>
-                <a class="dropdown-item d-flex align-items-center" href="{{ route('settings') }}">
-                    <i class="bi bi-gear"></i>
-                    <span>Account Settings</span>
-                </a>
-            </li>
-            <li>
-                <hr class="dropdown-divider">
-            </li>
-            <li>
-                <a class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}">
-                    <i class="bi bi-box-arrow-right"></i>
-                    <span>Sign Out</span>
-                </a>
-            </li>
-        </ul>
-    @else
-        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="{{ route('login') }}">
-            <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2">Đăng Nhập</span>
-        </a>
-    @endif
-</li>
+            <span class="d-none d-md-block dropdown-toggle ps-2">Farmer</span>
+          </a><!-- End Profile Iamge Icon -->
 
+          <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+            <li class="dropdown-header">
+              <h6>Meta</h6>
+              <span>Famer</span>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                <i class="bi bi-person"></i>
+                <span>My Profile</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
+                <i class="bi bi-gear"></i>
+                <span>Account Settings</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
+                <i class="bi bi-question-circle"></i>
+                <span>Need Help?</span>
+              </a>
+            </li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+
+            <li>
+              <a class="dropdown-item d-flex align-items-center" href="#">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+              </a>
+            </li>
+
+          </ul><!-- End Profile Dropdown Items -->
+        </li><!-- End Profile Nav -->
 
       </ul>
     </nav><!-- End Icons Navigation -->
