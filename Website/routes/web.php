28decdeb25b86811e844f5512fb7ea4583d54e4b
huuthocse <huutho.cse@gmail.com>
2024-10-30 17:56:35 +0700

    udp

--- Website/routes/web.php
+++ Website/routes/web.php
@@ -27,7 +27,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/admin', [AdminController::class, 'getView'])->name('admin.view');
     Route::post('/admin/add', [AdminController::class, 'addUser'])->name('user.add');
-    Route::post('/goats', [GoatController::class, 'addGoat'])->name('goat.add');
+    Route::post('/goats', [GoatController::class, 'addGoat'])->name('goats.add');
 
 });
 
