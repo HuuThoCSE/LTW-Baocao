06e404c34c7f64d970fb0e2c054b0bf33117c765
HoaiSang789 <sangv123543@gmail.com>
2024-10-30 17:05:39 +0700

    .

--- Website/resources/views/goat/dashboard.blade.php
+++ Website/resources/views/goat/dashboard.blade.php
@@ -4,27 +4,32 @@
 Danh sách dê
 @endsection
 
-@section('content')
+@section('contents')
 <h1>List of Goats</h1>
-<table class="table">
-    <tr>
-        <th>Code</th>
-        <th>Tên</th>
-        <th>Tuổi</th>
-        <th>Origin</th>
-    </tr>
-    <ul>
-    @foreach($listgoats as $goat)
-    
-        <tr>
-            <td>{{ $goat->goat_id }}</td>
-            <td>{{ $goat->goat_name }}</td>
-            <td>{{ $goat->goat_age }}</td>
-            <td>{{ $goat->origin }}</td>
-        </tr>
-   
+<div class="table-responsive">
+    <table class="table table-bordered">
+        <thead>
+            <tr align='center'>
+                <th>Code</th>
+                <th>Tên</th>
+                <th>Tuổi</th>
+                <th>Origin</th>
+                <th colspan="2">Operation</th>
+            </tr>
+        </thead>
+     
+        @foreach($listgoats as $goat)
         
-    @endforeach
-    </ul>
-</table>
+            <tr>
+                <td>{{ $goat->goat_id }}</td>
+                <td>{{ $goat->goat_name }}</td>
+                <td>{{ $goat->goat_age }}</td>
+                <td>{{ $goat->origin }}</td>
+            </tr>
+    
+            
+        @endforeach
+       
+    </table>
+</div>
 @endsection
