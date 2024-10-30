d60829bf33b68aa0cf673dd9b309d2e6cc59659e
huuthocse <huutho.cse@gmail.com>
2024-10-30 17:50:00 +0700

    Udp

--- Website/resources/views/goats/show.blade.php
+++ Website/resources/views/goats/show.blade.php
@@ -15,18 +15,16 @@ Bảng điều khiển
     </nav>
 </div>
 
-<h1>Thông tin chi tiết của dê: {{ $goat->name }}</h1>
+<h1>Thông tin chi tiết của dê: {{ $goat->goat_name }}</h1>
 <div class="card mb-3">
-    <div class="card-body">
-        <p><strong>Tên:</strong> {{ $goat->name }}</p>
-        <p><strong>Tuổi:</strong> {{ $goat->age }} năm</p>
-        <p><strong>Giống:</strong> {{ $goat->breed }}</p>
-        <p><strong>Cân nặng:</strong> {{ $goat->weight }} kg</p>
-        <p><strong>Tình trạng sức khỏe:</strong> {{ $goat->health_status }}</p>
-        <p><strong>Trạng thái tiêm chủng:</strong> {{ $goat->vaccination_status }}</p>
-    </div>
+    <p><strong>Tên:</strong> {{ $goat->goat_name }}</p>
+    {{-- <p><strong>Tuổi:</strong> {{ $goat->goat_age }} năm</p> --}}
+    {{-- <p><strong>Giống:</strong> {{ $goat->goat_breed }}</p> --}}
+    {{-- <p><strong>Cân nặng:</strong> {{ $goat->goat_weight }} kg</p> --}}
+    {{-- <p><strong>Tình trạng sức khỏe:</strong> {{ $goat->health_status }}</p> --}}
+    {{-- <p><strong>Trạng thái tiêm chủng:</strong> {{ $goat->vaccination_status }}</p> --}}
 </div>
-<a href="{{ route('goats.index') }}" class="btn btn-primary">Quay lại danh sách dê</a>
+<a href="{{ route('goats.listgoat') }}" class="btn btn-primary">Quay lại danh sách dê</a>
 
 <!-- End Page Title -->
 
