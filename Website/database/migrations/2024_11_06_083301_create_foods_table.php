8c5a13f31ccaf40fa6ceedc9c9689c4047efe273
HuuThoCSE <huutho.cse@gamil.com>
2024-12-13 12:12:31 +0700

    Udp

--- Website/database/migrations/2024_11_06_083301_create_foods_table.php
+++ Website/database/migrations/2024_11_06_083301_create_foods_table.php
@@ -16,16 +16,20 @@ return new class extends Migration
             $table->string('food_code', 8); // Mã thức ăn
             $table->string('food_name_vn', 250); // Tên thức ăn tiếng Việt
             $table->string('food_name_el', 250); // Tên thức ăn bằng ngôn ngữ khác
+            $table->foreignId('farm_id')
+                ->nullable()
+                ->constrained('farms', 'farm_id')
+                ->onDelete('cascade');
             $table->string('expiry_date', 250); // Ngày hết hạn
             $table->foreignId('type_food_id') // Khóa ngoại liên kết đến food_types
                 ->constrained('type_foods', 'type_food_id') // Chỉ định bảng và cột khóa chính
                 ->onDelete('cascade'); // Khi xóa loại thức ăn, thức ăn liên quan cũng bị xóa
             $table->timestamps(); // created_at và updated_at
         });
-       
+
     }
-    
-    
+
+
     /**
      * Reverse the migrations.
      */
